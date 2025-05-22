f1dd7fc5ecf1f02cc5d16a6ea7c5b11075737d6b
Olaitan Adedokun <kenzdex@gmail.com>
2025-05-22 17:15:52 +0100

    console.log all the variables and their types

---
+++ exercise_1.js
@@ -0,0 +1,18 @@
+const name = "Olaitan";
+const age = 20;
+const isStudent = true;
+const address = {
+  city: "Lagos",
+  country: "Nigeria",
+};
+const hobbies = ["sleeping", "watching movies", "coding", "travelling"];
+let undefinedVariable;
+const nullVariable = null;
+
+console.log(`The type for name is ${typeof name}`);
+console.log(`The type for age is ${typeof age}`);
+console.log(`The type for isStudent is ${typeof isStudent}`);
+console.log(`The type for address is ${typeof address}`);
+console.log(`The type for hobbies is ${typeof hobbies}`);
+console.log(`The type for undefinedVariable is ${typeof undefinedVariable}`);
+console.log(`The type for nullVariable is ${typeof nullVariable}`);
